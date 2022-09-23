35e53e3a10000a1af57975101485f3dfe5c42f98
PrathimaGopal <70705974+PrathimaGopal@users.noreply.github.com>
2022-09-22 21:52:12 -0700

    Revert "dynamic graph from aws data"

--- smartApartment/frontend/src/app/UserPool.js
+++
@@ -1,8 +0,0 @@
-import { CognitoUserPool } from "amazon-cognito-identity-js";
-
-const poolData = {
-    UserPoolId: "us-east-1_bcvbX1ffR",
-    ClientId: "65a7usugddmnq3u4rm7a3u42cs"
-}
-
-export default new CognitoUserPool(poolData);
