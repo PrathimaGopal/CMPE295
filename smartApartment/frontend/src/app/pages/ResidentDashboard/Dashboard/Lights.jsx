6d0f8cd29b406239cfda20f449e1eafeecc5fa96
Anjali <anjali.poddar0207@gmail.com>
2022-11-19 13:15:56 -0500

    added lights and index

--- smartApartment/frontend/src/app/pages/ResidentDashboard/Dashboard/Lights.jsx
+++ smartApartment/frontend/src/app/pages/ResidentDashboard/Dashboard/Lights.jsx
@@ -6,63 +6,59 @@ import { getAssetPath } from "app/utils/appHelpers";
 import { ASSET_IMAGES } from "app/utils/constants/paths";
 import { Switch } from "@mui/material";
 import axios from "axios";
-
+ 
 const Lights = (props) => {
-  const [value, setValue] = useState(props.dbValue);
-
-  const _onChange = (event) => {
-    setValue(event.target.checked ? 1 : 0);
-    console.log(props.label, ":", event.target.checked);
-  };
-
-  useEffect(() => {
-    // Make API Call
-    axios.defaults.headers.common = {
-      "X-API-Key": "pX5ri8vYyw7B3zwQeuPuD2CXJTN1vQT967oeBqsJ",
-    };
-
-    axios({
-      url: `https://oziv0d75z4.execute-api.us-west-1.amazonaws.com/Apartment1_API_gateway`,
-      method: "post",
-      data: {
-        deviceType: props.deviceType,
-        value: value,
-      },
-    })
-      .then((res) => {
-        if (res.status === 200) {
-          console.log("Success", res.data);
-        }
-      })
-      .catch((err) => {
-        console.log("Error:", err);
-      });
-  }, [props.label, value]);
-
-  return (
-    <JumboCardQuick bgColor={props.color}>
-      <Div sx={{ display: "flex", alignItems: "center" }}>
-        <img
-          alt={"Properties Icon"}
-          src={getAssetPath(
-            `${ASSET_IMAGES}/dashboard/projectIcon.svg`,
-            "46x46"
-          )}
-        />
-        <Div sx={{ ml: 2, flex: 1 }}>
-          <Typography color={"common.white"} variant={"h5"} mb={0.5}>
-            {props.label} 
-            <Switch
-              checked={value}
-              onChange={_onChange}
-              defaultChecked
-              color="warning"
-            />
-          </Typography>
-        </Div>
-      </Div>
-    </JumboCardQuick>
-  );
+ const [value, setValue] = useState(props.dbValue);
+ 
+ const _onChange = (event) => {
+   setValue(event.target.checked ? 1 : 0);
+   console.log(props.label, ":", event.target.checked);
+ };
+ 
+ const getData = async() => {
+   const {data} = await axios.get(`https://wtxngldocf.execute-api.us-west-1.amazonaws.com/prod/${props.label.toLowerCase()}`);
+ 
+   const payload = data?.data?.payload;
+ 
+   if(props.label === "Bedroom"){
+     const bedroomLight = data?.data?.filter((data) => data.DEVICE_TYPE === "BEDROOM_LIGHT")[0];
+     return setValue(bedroomLight?.DeviceAvailable > 0 ? true: false);
+   }
+ 
+   setValue(payload?.light === 1 ? true: false);
+ }
+ 
+ useEffect(() => {
+   getData();
+ }, [props.label]);
+ 
+ return (
+   <JumboCardQuick bgColor={props.color}>
+     <Div sx={{ display: "flex", alignItems: "center" }}>
+       <img
+         alt={"Properties Icon"}
+         src={getAssetPath(
+           `${ASSET_IMAGES}/dashboard/projectIcon.svg`,
+           "46x46"
+         )}
+       />
+       <Div sx={{ ml: 2, flex: 1 }}>
+         <Typography color={"common.white"} variant={"h5"} mb={0.5}>
+           {props.label} &nbsp;
+           <Switch
+             checked={value}
+             onChange={_onChange}
+             defaultChecked
+             color="warning"
+           />
+         </Typography>
+       </Div>
+     </Div>
+   </JumboCardQuick>
+ );
 };
-
+ 
 export default Lights;
+ 
+ 
+
